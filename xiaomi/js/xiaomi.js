4286918fc20c48f52d38566c817e045c659ae666
532758463 <532758463@qq.com>
2018-07-28 20:36:54 +0800

    小米图片切换及卡片效果更新

--- xiaomi/js/xiaomi.js
+++ xiaomi/js/xiaomi.js
@@ -4,10 +4,61 @@
 // 获取到section元素
 var ban = document.getElementById("banner");
 var img = ban.getElementsByTagName("img");
-console.log(img.length);
+// console.log(img.length);
 // 遍历img元素，并封装成切换图片的函数
+var index=0,size=img.length;
 function changeImg(){
 	for(var i=0;i<img.length;i++){
 		img[i].className="none";
 	}
+	img[index].className="active";
 }
+
+
+// 上一张图片切换
+var pre = document.getElementById("pre");
+pre.onclick=function(){
+	index--;
+	if(index>size-1){
+		index=0;	
+	}else if(index<0){
+		index=size-1;
+	}
+	changeImg();
+}
+// 下一张图片切换
+var next = document.getElementById("next");
+next.onclick = function(){
+	index++;
+	if(index>size-1){
+		index=0;
+	}else if(index<0){
+		index=size-1;
+	}
+	changeImg();
+}
+
+// 圆点导航
+var dots = document.querySelectorAll(".dots");
+// console.log(dots);
+// 使用匿名函数的目的是为了获得即时的i值
+for(var i=0;i<dots.length;i++){
+	(function(num){
+		dots[num].onclick=function(){
+		index=num;
+		changeImg();
+	}
+	})(i)
+}
+
+// 图片定时轮播
+setInterval(function(){
+	index++;
+	if(index>size-1){
+		index=0;
+	}else if(index<0){
+		index=size-1;
+	}
+	changeImg();
+},2000)
+
